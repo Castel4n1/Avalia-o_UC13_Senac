e6e0ecafa51243e3e76eb443ac42e61dd29ce064
Castelani <kaue1castelani@gmail.com>
2022-10-04 19:55:35 -0300

    Etapa 1 concluida

---
+++ CadastroAlunos/Contracts/IAlunoRepository.cs
@@ -0,0 +1,17 @@
+﻿using CadastroAlunos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroAlunos.Contracts
+{
+    public interface IAlunoRepository
+    {
+        Task<List<Aluno>> GetAlunos();
+        Task<Aluno> GetAlunoById(int id);
+        Task<Aluno> AddAluno(Aluno aluno);
+        Task<int> UpdateAluno(int id, Aluno alunoAlterado);
+        Task DeleteAluno(int id);
+    }
+}
---
+++ CadastroAlunos/Repository/AlunoRepository.cs
@@ -0,0 +1,61 @@
+﻿using CadastroAlunos.Contracts;
+using CadastroAlunos.Data;
+using CadastroAlunos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroAlunos.Repository
+{
+    public class AlunoRepository : IAlunoRepository
+    {
+        private readonly CadastroAlunosContext _context;
+
+        public AlunoRepository(CadastroAlunosContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Aluno>> GetAlunos()
+        {
+            return await _context.Aluno.ToListAsync();
+        }
+
+        public async Task<Aluno> GetAlunoById(int id)
+        {
+            return await _context.Aluno.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Aluno> AddAluno(Aluno aluno)
+        {
+
+            _context.Aluno.Add(aluno);
+            await _context.SaveChangesAsync();
+
+            return aluno;
+        }
+
+        public async Task<int> UpdateAluno(int id, Aluno alunoAlterado)
+        {
+            var aluno = await _context.Aluno.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (aluno == null)
+                return 0;
+
+            //cliente.AtualizaDados(clienteAlterado.Nome, clienteAlterado.Nascimeto, clienteAlterado.Email);
+
+            _context.Entry(aluno).State = EntityState.Modified;
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAluno(int id)
+        {
+            var aluno = await _context.Aluno.FirstOrDefaultAsync(c => c.Id == id);
+
+            _context.Aluno.Remove(aluno);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
