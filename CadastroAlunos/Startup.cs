0e3cb0e152f58dd0f0e833822aa5be877d6f4ea2
Castelani <kaue1castelani@gmail.com>
2022-10-04 19:33:10 -0300

    start up

--- CadastroAlunos/Startup.cs
+++ CadastroAlunos/Startup.cs
@@ -55,7 +55,7 @@ namespace CadastroAlunos
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Aluno}/{action=Index}/{id?}");
             });
         }
     }
